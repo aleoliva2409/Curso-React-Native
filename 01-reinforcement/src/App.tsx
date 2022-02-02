e91a31a7d51caa0cfa45d903bbd642850b8810f9
Alejandro Oliva <alejandro.oliva2409@gmail.com>
2022-02-02 01:58:35 -0300

    first commit

---
+++ 01-reinforcement/src/App.tsx
@@ -0,0 +1,12 @@
+import TiposBasicos from './components/TiposBasicos';
+
+const App = () => {
+  return (
+    <div className='mt-2'>
+      <h1>Introduccion a TS</h1>
+      <TiposBasicos />
+    </div>
+  );
+};
+
+export default App;
---
+++ 01-reinforcement/src/components/TiposBasicos.tsx
@@ -0,0 +1,9 @@
+const TiposBasicos = () => {
+  return (
+    <>
+      <h3>Tipos Basicos</h3>
+    </>
+  );
+};
+
+export default TiposBasicos;

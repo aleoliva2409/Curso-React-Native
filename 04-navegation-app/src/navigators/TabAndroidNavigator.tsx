a805966e287080ea3ad2464bdb67807ac9dd82e9
Alejandro Oliva <alejandro.oliva@aicore.com.ar>
2022-02-13 00:44:15 -0300

    commit prev icons intall

--- 04-navegation-app/src/navigators/TabAndroidNavigator.tsx
+++ 04-navegation-app/src/navigators/TabAndroidNavigator.tsx
@@ -24,7 +24,7 @@ const TabAndroidNavigator = () => {
             case 'Tab1Screen':
               iconName = 'T1';
               break;
-            case 'Tab2Screen':
+            case 'TopTabNavigator':
               iconName = 'T2';
               break;
             case 'StackNavigator':
--- 04-navegation-app/src/navigators/TopTabNavigator.tsx
+++ 04-navegation-app/src/navigators/TopTabNavigator.tsx
@@ -4,17 +4,68 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import ChatScreen from '../screens/ChatScreen';
 import ContactScreen from '../screens/ContactScreen';
 import AlbumScreen from '../screens/AlbumScreen';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { colors } from '../theme/appTheme';
+import { Text } from 'react-native';
 
 const Tab = createMaterialTopTabNavigator();
 
 const TopTabNavigator = () => {
+  const inset = useSafeAreaInsets();
+
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="ChatScreen" options={{ title: 'Chat'}} component={ChatScreen} />
-      <Tab.Screen name="ContactScreen" options={{ title: 'Contact'}} component={ContactScreen} />
-      <Tab.Screen name="AlbumScreen" options={{ title: 'Album'}} component={AlbumScreen} />
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: '#fff',
+      }}
+      screenOptions={({ route }) => ({
+        tabBarPressColor: colors.primary,
+        tabBarShowIcon: true,
+        tabBarIndicatorStyle: {
+          backgroundColor: colors.primary,
+        },
+        tabBarStyle: {
+          elevation: 0,
+          shadowColor: 'transparent', // ? para IOS
+        },
+        tabBarIcon: ({ color }) => {
+          let iconName: string = '';
+
+          switch (route.name) {
+            case 'ChatScreen':
+              iconName = 'Ch';
+              break;
+            case 'ContactScreen':
+              iconName = 'Ct';
+              break;
+            case 'AlbumScreen':
+              iconName = 'Al';
+              break;
+          }
+
+          return <Text style={{ color }}>{iconName}</Text>;
+        },
+      })}
+      style={{
+        marginTop: inset.top,
+      }}>
+      <Tab.Screen
+        name="ChatScreen"
+        options={{ title: 'Chat' }}
+        component={ChatScreen}
+      />
+      <Tab.Screen
+        name="ContactScreen"
+        options={{ title: 'Contact' }}
+        component={ContactScreen}
+      />
+      <Tab.Screen
+        name="AlbumScreen"
+        options={{ title: 'Album' }}
+        component={AlbumScreen}
+      />
     </Tab.Navigator>
   );
-}
+};
 
 export default TopTabNavigator;
